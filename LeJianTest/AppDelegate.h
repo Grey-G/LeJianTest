611b1aebfb482519122c53a0ab5afcddf7725eea
Grey <gh111133@163.com>
2020-10-19 17:35:52 +0800

    Initial Commit

---
+++ LeJianTest/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LeJianTest
+//
+//  Created by Guo Grey on 2020/10/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LeJianTest/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  LeJianTest
+//
+//  Created by Guo Grey on 2020/10/19.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
